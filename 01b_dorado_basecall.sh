7d25b136312268089624aeb1528e25cee602a774
frankharders <frank.harders@wur.nl>
2026-08-07 23:09:45 +0200

    Add --emit-moves option to basecaller command

--- 01b_dorado_basecall.sh
+++ 01b_dorado_basecall.sh
@@ -155,6 +155,7 @@ log "POD5-bestanden  : ${POD5_COUNT} (totaal ${POD5_SIZE})"
 CMD=("${DORADO}" basecaller
      --device cuda:all
      --barcode-both-ends
+     --emit-moves
      --kit-name "${KIT}"
      "${MODEL_SPEC}"
      "${IN_DIR}")
